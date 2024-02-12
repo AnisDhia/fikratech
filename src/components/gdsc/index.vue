cc56c2738492a9a1af1717ffbcb55dd916d540cf
AnisDhia <50758880+AnisDhia@users.noreply.github.com>
2024-02-12 19:31:17 +0100

    pew

--- src/components/gdsc/index.vue
+++ src/components/gdsc/index.vue
@@ -4,10 +4,25 @@
             <v-img src="@/assets/gdsc/gdsc1.svg" max-width="350"></v-img>
             <v-col>
                 <h1 class="">Who are GDSC Batna?</h1>
-                <p>Google Developer Student Clubs (GDSC) are community groups for college and university students interested
+                <p>
+                    Google Developer Student Clubs are community groups for college and
+                    university students interested in Google developer technologies.
+                    <!-- Google Developer Student Clubs (GDSC) are community groups for college and university students interested
                     in Google developer technologies. Students from all undergraduate or graduate programs with an interest
                     in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a
-                    peer-to-peer learning environment and build solutions for local businesses and their community.</p>
+                    peer-to-peer learning environment and build solutions for local businesses and their community. -->
+                </p>
+                <div class="">
+                    <v-btn color="transparent" icon size="x-large" flat>
+                        <v-icon size="x-large" color="#D27849" icon="custom:insta"></v-icon>
+                    </v-btn>
+                    <v-btn color="transparent" icon size="x-large" flat>
+                        <v-icon size="x-large" color="#D27849" icon="custom:linkedin"></v-icon>
+                    </v-btn>
+                    <v-btn color="transparent" icon size="x-large" flat>
+                        <v-icon size="x-large" color="#D27849" icon="custom:facebook"></v-icon>
+                    </v-btn>
+                </div>
             </v-col>
         </v-row>
     </section>
@@ -32,5 +47,4 @@ p {
     color: #333333;
     size: 2rem;
 }
-
 </style>
